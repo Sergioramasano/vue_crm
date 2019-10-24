23ddaf675531026bc990c11f6af214391acfdf77
Sergioramasano <razumenko.serg@gmail.com>
2019-10-24 16:43:27 +0300

    10 lesson completed

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
 import info from './info'
+import category from './category'
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -22,12 +23,13 @@ export default new Vuex.Store({
   actions:{
     async fetchCurrency(){
       const key = process.env.VUE_APP_FIXER
-      const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&base = USD&symbols=UAH,USD,EUR`)
+      const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=UAH,USD,EUR`)
       return await  res.json()
     }
   },
 modules:{
   auth,
-  info
+  info,
+  category
 }
 })
